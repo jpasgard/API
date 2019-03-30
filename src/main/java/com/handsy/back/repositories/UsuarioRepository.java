14c9bf9adef0de314ba4460b2b720b53dd3aa3d1
eduardo <eduardocco11@gmail.com>
2019-03-30 15:39:20 -0300

    App-Iniciando back-end do projeto

---
+++ src/main/java/com/handsy/back/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.handsy.back.repositories;
+
+import com.handsy.back.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository  extends JpaRepository<Usuario, Integer> {
+}
